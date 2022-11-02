f5ef00e6d66b7ab1ffdfe635777fdce97dcdd569
andres-mestra <andresmestra11a@gmail.com>
2022-11-01 19:13:56 -0500

    build: define base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/oraculo/' : '/',
+  plugins: [react()],
+}))
