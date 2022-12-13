6e54fbfae70f00a5a92a5d27ac7f1152a50c47a4
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-12-13 18:33:12 -0500

    refactor: capitalize text toggle button theme mode

--- src/components/ToggleThemeMode.jsx
+++ src/components/ToggleThemeMode.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Box, IconButton } from '@mui/material'
+import { Box, IconButton, Typography } from '@mui/material'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 import Brightness7Icon from '@mui/icons-material/Brightness7'
 import { useTheme } from '@mui/material/styles'
@@ -20,7 +20,9 @@ export const ToggleThemeMode = () => {
         padding: 1,
       }}
     >
-      {theme.palette.mode} mode
+      <Typography textTransform="capitalize">
+        {theme.palette.mode} mode
+      </Typography>
       <IconButton
         sx={{ ml: 1 }}
         onClick={colorMode.toggleColorMode}
