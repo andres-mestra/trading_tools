7e9cd427773b8c296629d6340e21538c462d0175
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-12-16 02:25:17 -0500

    feat: get coins that meet the two to one

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -227,6 +227,57 @@ export function Home() {
     })
   }
 
+  const handleGetTwoToOne = async () => {
+    const resp = await fetch('https://fapi.binance.com/fapi/v1/exchangeInfo')
+    const data = await resp.json()
+    const { symbols } = data
+    const newSymbols = []
+    symbols.forEach((s) => {
+      const { symbol, contractType, status } = s
+      const isUsdt = symbol.includes('USDT')
+      const isPerpetual = contractType === 'PERPETUAL'
+      const isTrading = status === 'TRADING'
+      if (isUsdt && isPerpetual && isTrading)
+        newSymbols.push(symbol.replace('USDT', ''))
+    })
+
+    const longs = []
+    const shorts = []
+
+    for (const symbol of newSymbols) {
+      console.log(symbol)
+      const points = await getEntryPoints(symbol)
+      if (points !== undefined) {
+        const { shortPoints, longPoints } = points
+
+        const ratioLong =
+          (shortPoints.entry - longPoints.entry) /
+          (longPoints.entry - longPoints.buyBack)
+        const ratioShort =
+          (shortPoints.entry - longPoints.entry) /
+          (shortPoints.buyBack - shortPoints.entry)
+
+        if (ratioLong >= 1.66)
+          longs.push({
+            symbol,
+            ratio: ratioLong,
+            tp: shortPoints.entry,
+            ...longPoints,
+          })
+        if (ratioShort >= 1.66)
+          shorts.push({
+            symbol,
+            ratio: ratioShort,
+            tp: longPoints.entry,
+            ...shortPoints,
+          })
+      }
+    }
+
+    console.log('MONEDAS DOS A UNO, RECOMENDADAS')
+    console.log({ longs, shorts })
+  }
+
   useEffect(() => {
     if (coins.length) {
       socketsRef.current = generateSocket()
@@ -294,6 +345,9 @@ export function Home() {
                 ref={refInputImport}
                 onChange={handleImportPoints}
               />
+            </Button>
+            <Button variant="outlined" onClick={handleGetTwoToOne}>
+              Dos / Uno
             </Button>
             <Button variant="outlined" onClick={handleNotification}>
               notificar
