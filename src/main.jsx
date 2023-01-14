67a92a8b0ccf855ae2a27dffb72078c37151c7f1
andresmestra <andresmestra11a@gmail.com>
2023-01-13 23:44:02 -0500

    fix: root router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename="/trading_tools">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
