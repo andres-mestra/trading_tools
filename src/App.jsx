a8a7d33e88f920415b7654e2efb3b5a748296d5f
andresmestra <andresmestra11a@gmail.com>
2023-01-14 00:17:05 -0500

    fix: define router hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { GlobalStyles, ThemeGlobal } from './theme/ThemeGlobal'
 import { Router } from './Router'
+import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeGlobal>
       <GlobalStyles />
-      <Router />
+      <HashRouter>
+        <Router />
+      </HashRouter>
     </ThemeGlobal>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
