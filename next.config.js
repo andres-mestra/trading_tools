ebf4d68eb41ed371b22a9fbb29c0532cf40525fe
andresmestra <andresmestra11a@gmail.com>
2023-01-16 15:41:38 -0500

    build: define web3 for server and client

--- next.config.js
+++ next.config.js
@@ -2,10 +2,19 @@ export default {
   experimental: {
     appDir: true,
   },
-  webpack: (config, options) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'mkdirp-promise': '/helpers/mkdirpPromise.js',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        web3: '/node_modules/web3/dist/web3.min.js',
+      }
+    }
+
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'mkdirp-promise': '/helpers/mkdirpPromise.js',
+      }
     }
 
     return config
