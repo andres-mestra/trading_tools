da9d772739328ee141097de2de0b8c459bc9286f
andresmestra <andresmestra11a@gmail.com>
2023-01-27 19:03:08 -0500

    refactor: define size md

--- hooks/useMediaQueryMd.js
+++ hooks/useMediaQueryMd.js
@@ -1,3 +1,4 @@
 import { useMediaQuery } from '@mui/material'
 
-export const useMediaQueryMd = () => useMediaQuery('(max-width:1400px)')
+export const useMediaQueryMd = () =>
+  useMediaQuery('(min-width:1000px)', { noSsr: true })
