20c47bf2025303a4d1f3da73665bddc79372e4a0
andresmestra <andresmestra11a@gmail.com>
2023-01-30 13:48:38 -0500

    feat: add vercel analytics

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -3,6 +3,7 @@
 import { GlobalStyles, ThemeGlobal } from 'theme/ThemeGlobal'
 import { Web3Provider } from 'config/web3'
 import { AuthProvider } from 'context/AuthContext'
+import { AnalyticsWrapper } from 'components/AnalyticsWrapper'
 
 export const Providers = ({ children }) => {
   return (
@@ -11,6 +12,7 @@ export const Providers = ({ children }) => {
         <ThemeGlobal>
           <GlobalStyles />
           {children}
+          <AnalyticsWrapper />
         </ThemeGlobal>
       </AuthProvider>
     </Web3Provider>
